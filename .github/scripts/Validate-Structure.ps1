6c082a3e542344c2d1acfdc30f140a026ebc947b
insyri <60794909+insyri@users.noreply.github.com>
2022-04-10 17:55:45 -0400

    Create Validate-Structure.ps1

---
+++ .github/scripts/Validate-Structure.ps1
@@ -0,0 +1,15 @@
+If (!Test-Path -Path staging         -PathType Container) { exit 1 }
+If (!Test-Path -Path production      -PathType Container) { exit 1 }
+If (!Test-Path -Path .github         -PathType Container) { exit 1 }
+If (!Test-Path -Path staging/keepalive    -PathType Leaf) { exit 1 }
+If (!Test-Path -Path production/keepalive -PathType Leaf) { exit 1 }
+
+$list = 'staging', 'production'
+
+Foreach ($f in $list) {
+    Get-ChildItem -Path $f -Exclude keepalive |
+    ForEach-Object {
+        If ($_.Mode -eq "d----")      { exit 1 }
+        If ($_.Extension -ne ".diff") { exit 1 }
+    }
+}
