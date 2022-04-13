a65cc274df37ef2e02e9a344801ff75ddce5b97b
insyri <60794909+insyri@users.noreply.github.com>
2022-04-12 21:30:07 -0400

    start on map request

--- .github/scripts/Move-To-Production.ps1
+++
@@ -1,8 +0,0 @@
-$list = 'staging', 'production'
-$L = Get-Location
-ForEach ($f in $list) {
-    Get-ChildItem -Path "staging" -Filter *.diff |
-    ForEach-Object {
-        Move-Item -Path $_.DirectoryName -Destination "$L\production"
-    }
-}
---
+++ .github/scripts/Request-Map-Model.ps1
@@ -0,0 +1,11 @@
+Function Request-Map-Model {
+  Param(
+    [Parameter(Mandatory = $true , Position = 0, Type = "string")] $ROBLOSECURITY,
+    [Parameter(Mandatory = $true , Position = 1, Type = "string")] $APIKEY,
+    [Parameter(Mandatory = $false, Position = 3, Type = "string")] $MAP,
+    [Parameter(Mandatory = $false, Position = 4, Type = "string")] $Patch
+  )
+  process {
+    # Validation Process
+  }
+}
