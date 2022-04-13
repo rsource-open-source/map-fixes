dda77cde3e974bb9706560513ea246d32c0f5d5a
insyri <60794909+insyri@users.noreply.github.com>
2022-04-13 07:14:16 -0400

    Normalize caps [ci skip]

--- .github/scripts/Request-Map-Model.ps1
+++ .github/scripts/Request-Map-Model.ps1
@@ -3,7 +3,7 @@ Function Request-Map-Model {
     [Parameter(Mandatory = $true , Position = 0, Type = "string")] $ROBLOSECURITY,
     [Parameter(Mandatory = $true , Position = 1, Type = "string")] $APIKEY,
     [Parameter(Mandatory = $false, Position = 3, Type = "string")] $MAP,
-    [Parameter(Mandatory = $false, Position = 4, Type = "string")] $Patch
+    [Parameter(Mandatory = $false, Position = 4, Type = "string")] $PATCH
   )
   process {
     # Validation Process
