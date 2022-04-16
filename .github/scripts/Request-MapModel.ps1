458f5970e8349d109cd7fe29379ebc2eb5d8bd11
insyri <60794909+insyri@users.noreply.github.com>
2022-04-16 17:45:59 -0400

    syncng

--- .github/scripts/Request-MapModel.ps1
+++ .github/scripts/Request-MapModel.ps1
@@ -1,28 +1,17 @@
 Function Request-MapModel {
   [CmdletBinding()]
   Param(
-    [Parameter(Mandatory = $true, Position = 0)] [string]$MAP,
-    [Parameter(Mandatory = $false, Position = 1)] [string]$PATCH
+    [Parameter(Mandatory = $true, Position  = 0)] [string]$ROSEC,
+    [Parameter(Mandatory = $true, Position  = 1)]
+    [ValidatePattern("[0-9]+")]                   [string]$MAP,
+    [Parameter(Mandatory = $false, Position = 2)] [string]$PATCH
   )
 
-  Write-Verbose "Checking map input type"
+  Write-Verbose "Invoking the Asset Delivery Roblox API: /v1/assetId/$Map"
+  $res = Invoke-RestMethod -Uri "https://assetdelivery.roblox.com/v1/assetId/${FoundMap}" -Method "Get" -Headers "Accept: application/json"
 
-  If ($MAP -imatch "[0-9]+") {
-    Write-Verbose "Identified map input is ID"
-    $MAP = [regex]::split($MAP, ": ?")
-
-    Write-Verbose "Invoking the Asset Delivery Roblox API: /v1/assetId/$Map"
-    $res = Invoke-RestMethod -Uri "https://assetdelivery.roblox.com/v1/assetId/${FoundMap}" -Method "Get" -Headers "Accept: application/json"
-
-    Write-Verbose "Checking for errors returned from the API"
-    If ($res.GetType().Name -eq "PSCustomObject" -and $null -ne $res["message"]) {
-      If ($res["message"] -match
-        "(No API key found in request|Invalid authentication credentials)") {
-        Write-Error $res["message"] -TargetObject $res -Category AuthenticationError
-      }
-    }
-    Else { Write-Verbose "✔ No errors (i think)" }
-  }
+  Write-Verbose "Checking for errors returned from the API"
+}
 
   # First, look for the map model in the strafesnet account
   # OR grab it from the api via id and get the model
