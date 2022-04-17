aa2d2aa27d9b4eb5edf5b2e210f5530610c3573d
insyri <60794909+insyri@users.noreply.github.com>
2022-04-17 08:30:22 -0400

    g it

--- .github/scripts/Validate-Structure.ps1
+++ .github/scripts/Validate-Structure.ps1
@@ -2,6 +2,8 @@ If (!(Test-Path -Path staging         -PathType Container)) { Exit 1 }
 If (!(Test-Path -Path production      -PathType Container)) { Exit 1 }
 If (!(Test-Path -Path .github         -PathType Container)) { Exit 1 }
 If (!(Test-Path -Path staging/keepalive    -PathType Leaf)) { Exit 1 }
+If (!(Test-Path -Path .gitattributes      - PathType Leaf)) { Exit 1 }
+If (!(Test-Path -Path .gitignore           -PathType Leaf)) { Exit 1 }
 If (!(Test-Path -Path production/keepalive -PathType Leaf)) { Exit 1 }
 
 $list = 'staging', 'production'
