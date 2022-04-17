6699b5180bf32da8fd4b4d72048768cd16884b7c
insyri <60794909+insyri@users.noreply.github.com>
2022-04-17 17:31:06 -0400

    psanalyzer was bugging me

--- .github/scripts/Compile-RbxUtil.ps1
+++ .github/scripts/Compile-RbxUtil.ps1
@@ -1,13 +1,13 @@
 mkdir tmp
-cd tmp
+Set-Location tmp
 git clone https://github.com/rojo-rbx/rbx-dom.git
-cd rbx-dom
+Set-Location rbx-dom
 cargo build -p rbx_util --release
-mv target\release\rbx_util.exe ..\.github\bin
+Move-Item target\release\rbx_util.exe ..\.github\bin
 
 # end
 # start git
 
-git add .\.github\bin\rbx_util.exe
-git commit -m "rbx_util"
-git push origin main # or master
+# git add .\.github\bin\rbx_util.exe
+# git commit -m "rbx_util"
+# git push origin main # or master
