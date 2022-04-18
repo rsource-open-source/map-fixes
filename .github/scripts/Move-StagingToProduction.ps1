daf029dec238a1dd98d7afc7fedd6644734b7dc6
insyri <60794909+insyri@users.noreply.github.com>
2022-04-17 20:57:43 -0400

    1 line ez

--- .github/scripts/Move-StagingToProduction.ps1
+++ .github/scripts/Move-StagingToProduction.ps1
@@ -1,8 +1,1 @@
-$list = 'staging', 'production'
-$L = Get-Location
-ForEach ($f in $list) {
-    Get-ChildItem -Path "staging" -Filter *.diff |
-    ForEach-Object {
-        Move-Item -Path $_.DirectoryName -Destination "$L\production"
-    }
-}
+Get-ChildItem "staging" -Filter "*.diff" | Move-Item -Destination "production"
