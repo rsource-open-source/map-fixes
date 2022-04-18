6500a82d8b01353c2a65a7f70781190d4c63b5f1
insyri <60794909+insyri@users.noreply.github.com>
2022-04-17 22:44:54 -0400

    add readme + folders => files

--- .github/scripts/Validate-Structure.ps1
+++ .github/scripts/Validate-Structure.ps1
@@ -1,12 +1,12 @@
 $ExpectedFolders = 'staging', 'production', '.github'
-$ExpectedFiles = '.gitignore', '.gitattributes', 'staging\keepalive', 'production\keepalive'
+$ExpectedFiles = '.gitignore', '.gitattributes', 'staging\keepalive', 'production\keepalive', 'README.md'
 $ExpectedFileRegex = "(.+\.(patch|diff)|keepalive)"
 
 Get-ChildItem -Attributes "Directory" -Name | ForEach-Object {
   If (!($_ -in $ExpectedFolders)) { Throw "Unexpected folder: $_, expected folders: $ExpectedFolders"} }
 
 Get-ChildItem -Attributes "Archive" -Name | ForEach-Object {
-  If (!($_ -in $ExpectedFiles)) { Throw "Unexpected file: $_, expected files: $ExpectedFolders"} }
+  If (!($_ -in $ExpectedFiles)) { Throw "Unexpected file: $_, expected files: $ExpectedFiles"} }
 
 Get-ChildItem $ExpectedFolders[0,1] -Name | ForEach-Object {
     If ($_ -notmatch $ExpectedFileRegex) { Throw "Unexpected file: $_, expected file RegEx: $ExpectedFileRegex" }
